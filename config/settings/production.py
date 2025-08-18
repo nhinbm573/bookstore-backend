4f5461dca77747f2e84e958ea1f55a3960d8f9ea
nhinbm <nhi.nguyen@eastagile.com>
2025-08-18 15:33:13 +0700

    hotfix: settings

--- config/settings/production.py
+++ config/settings/production.py
@@ -23,18 +23,6 @@ DATABASES = {
 }
 
 
-# Security settings
-SECURE_SSL_REDIRECT = config("SECURE_SSL_REDIRECT", default=True, cast=bool)
-SESSION_COOKIE_SECURE = config("SESSION_COOKIE_SECURE", default=True, cast=bool)
-CSRF_COOKIE_SECURE = config("CSRF_COOKIE_SECURE", default=True, cast=bool)
-SECURE_BROWSER_XSS_FILTER = True
-SECURE_CONTENT_TYPE_NOSNIFF = True
-X_FRAME_OPTIONS = "DENY"
-SECURE_HSTS_SECONDS = config("SECURE_HSTS_SECONDS", default=31536000, cast=int)
-SECURE_HSTS_INCLUDE_SUBDOMAINS = True
-SECURE_HSTS_PRELOAD = True
-
-
 # Email configuration
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = config("EMAIL_HOST", default="smtp.gmail.com")
@@ -46,10 +34,7 @@ DEFAULT_FROM_EMAIL = config("DEFAULT_FROM_EMAIL", default="noreply@yourdomain.co
 
 
 # Production apps
-PROD_APPS = [
-    "gunicorn",
-    "storages",
-]
+PROD_APPS = []
 
 INSTALLED_APPS += PROD_APPS
 
@@ -59,20 +44,3 @@ CORS_ALLOWED_ORIGINS = [
     config("FRONTEND_DOMAIN", default="https://bookstore-frontend-silk.vercel.app"),
 ]
 CORS_ALLOW_CREDENTIALS = True
-
-
-# Logging
-LOGGING = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "handlers": {
-        "file": {
-            "level": "ERROR",
-            "class": "logging.FileHandler",
-            "filename": BASE_DIR / "logs" / "django_errors.log",
-        },
-    },
-    "root": {
-        "handlers": ["file"],
-    },
-}
